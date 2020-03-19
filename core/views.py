2b7f73ca1e06d2623d42efcc180ac09583400f91
Marsha <marshakshearin@gmail.com>
2020-03-19 14:18:20 -0400

    data shows up on flashcard.html

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Deck
 
 
 def home(request):
@@ -9,5 +9,6 @@ def home(request):
 
 
 def flashcards(request):
-    context = {}
+    qs = Deck.objects.all()
+    context = {'decks': qs}
     return render(request, 'core/flashcards.html', context)
